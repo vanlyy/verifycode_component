8b95c4dcca6ba4b7b9d7780f7756bb70508d59cc
vanlyy <383878958@qq.com>
2020-02-12 17:39:54 +0800

    Mybatis进阶

--- mybatis_demo/src/com/how2java/TestMybatis.java
+++ mybatis_demo/src/com/how2java/TestMybatis.java
@@ -1,31 +1,37 @@
 package com.how2java;
-  
+ 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
  
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
  
-import com.how2java.pojo.Product;
-  
+import com.how2java.pojo.Category;
+ 
 public class TestMybatis {
-  
+ 
     public static void main(String[] args) throws IOException {
         String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        SqlSession session = sqlSessionFactory.openSession();
-  
-        List<Product> ps = session.selectList("listProduct");
-        for (Product p : ps) {
-            System.out.println(p+" 对应的分类是 \t "+ p.getCategory());
-        }
+        SqlSession session1 = sqlSessionFactory.openSession();
  
-        session.commit();
-        session.close();
-  
+        Category c1 = session1.selectOne("getCategory", 6);
+        System.out.println(c1);
+        Category c2 = session1.selectOne("getCategory", 6);
+        System.out.println(c2);
+ 
+        session1.commit();
+        session1.close();
+         
+        SqlSession session2 = sqlSessionFactory.openSession();
+        Category c3 = session2.selectOne("getCategory", 6);
+        System.out.println(c3);    
+        session2.commit();
+        session2.close();
+         
     }
+ 
 }
